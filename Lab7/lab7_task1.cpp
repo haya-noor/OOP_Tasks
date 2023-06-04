e72992ab146d28ca0e282ae021fb7e436f3be0a0
Haya Noor <70140385+Haya-Noor@users.noreply.github.com>
2023-06-04 21:28:56 +0500

    Senior_Junior.cpp

--- Lab7/lab7_task1.cpp
+++ Lab7/lab7_task1.cpp
@@ -1,29 +1,36 @@
+
 #include<iostream>
 using namespace std;
 
 class person
 {
 	string name;
-	int regNo;
+	int id;
 	static int count;
 public:
 	person()
 	{
 		name = " ";
-		regNo = 0;
+		id = 0;
 		count++;
 	}
-	friend ostream& operator<(ostream& out, person p)
+	friend ostream& operator<<(ostream& out, person& p)
 	{
+        out<<p.name<<" "<<p.id<<endl;
+        return out;
 	}
-	friend istream& operator>(istream& in, person p)
+	friend istream& operator>>(istream& in, person& p)
 	{
 		cout << "Enter Name and ID:";
-		in >> p.name >> p.regNo;
+		in >> p.name >> p.id;
 		return in;
 	}
-	friend class date;
+    static int counter()
+    {
+        return count;
+    }
 };
+
 int person::count = 0;
 
 class date
@@ -35,28 +42,25 @@ public:
 	{
 		day = month = year = 0;
 	}
-	~date(){}
 	friend ostream& operator<<(ostream& out, date& d)
 	{
+        out<<d.year<<"-"<<d.month<<"-"<<d.day<<endl;
+        return out;
 	}
-	friend istream& operator>(istream& in, date& d)
+	friend istream& operator>>(istream& in, date& d)
 	{
-		cout << "Enter joining date(yy/mm/dd):";
+		cout << "Enter joining date(yyyy mm dd):";
 		in >> d.year >> d.month >> d.day;
 		return in;
 	}
 	bool operator <(date& d)
 	{
-		if (this->year < d.year)
-		{
-			cout << d.p.name1 << " is junior to " << d.p.name2 << " because " << this->year << "-" << this->month << "-" << this->day << " is earlier than";
-			cout << d.year << "-" << d.month << d.day << endl;
-		}
+		if (this->year !=  d.year)
+            return this->year < d.year;
+        else if(this->month != d.month)
+            return this->month < d.month;
 		else
-		{
-			cout << d.p.name1 << " is senior to " << d.p.name2 << " because " << this->year << "-" << this->month << "-" << this->day << " is earlier than";
-			cout << d.year << "-" << d.month << d.day << endl;
-		}
+			return this->day < d.day;
 	}
 	friend class person;
 };
@@ -64,8 +68,19 @@ public:
 int main()
 {
 	person p1, p2;
-	cin>>p1;
-	cout<<p1;
-	cin>>p2;
-	cout<<p2;
+	date d1, d2;
+
+    cout<<"\nEnter person No. 1 Data:  \n";
+    cin>>p1>>d1;
+    
+    cout<<"\nEnter person NO. 2 Data: \n";
+    cin>>p2>>d2;
+
+    if(d1 < d2)
+        cout<< p1 <<" is senior to "<< p2 <<" since "<< d1 <<" is earlier to "<< d2 << endl;
+    else 
+        cout<< p2 <<" is senior to "<< p1 <<" since "<< d2 <<" is earlier to "<< d1 << endl; 
+
+    cout<<"Total persons: "<< person::counter <<endl;
+    return 0;      
 }
