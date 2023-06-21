34ddd52f76ad8e8e266db0d66101238668dd19d7
Haya Noor <70140385+haya-noor@users.noreply.github.com>
2023-06-21 23:32:45 +0500

    swap_with_two_variables.cpp

---
+++ swap_with_two_variables.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+
+class swappp
+{
+    int a,b;
+    public:
+    swappp()
+    {
+        a = 0;
+        b = 0;
+    }
+
+    void setter()
+    {
+        cout<<"Enter value of a: ";
+        cin>>a;
+        cout<<"\nEnter value of b: ";
+        cin>>b;
+    }
+
+    void getter()
+    {
+        cout<<"a = "<<a<<endl;
+        cout<<"b = "<<b<<endl;
+    }
+
+    void swapppp()
+    {
+        a = a + b;
+        b = a - b;
+        a = a - b;
+    }
+
+    void display()
+    {
+        cout<<"Before swapping\n";
+        cout<<"a = "<<a<<endl;
+        cout<<"b = "<<b<<endl;
+
+        swapppp();
+        cout<<"After swapping\n";
+        cout<<"a = "<<a<<endl;
+        cout<<"b = "<<b<<endl;
+    }
+};
+
+int main()
+{
+    swappp s;
+
+    s.setter();
+    s.display();
+    return 0;
+}
